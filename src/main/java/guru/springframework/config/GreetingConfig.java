f73122cf4139be50eef768c2005772eceb2888b5
attikovacs <attikovacs@gmail.com>
2017-11-03 22:41:50 +0100

    Moved annotation to Java configuration file. Introduced ServiceFactory bean.

---
+++ src/main/java/guru/springframework/config/GreetingConfig.java
@@ -0,0 +1,40 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+
+@Configuration
+public class GreetingConfig {
+
+	@Bean
+	public GreetingServiceFactory getGreetingServiceFactory(GreetingRepository greetingRepository) {
+		return new GreetingServiceFactory(greetingRepository);
+	}
+	
+	@Bean
+	@Primary
+	@Profile({"default", "en"})
+	public GreetingService getGreetingService(GreetingServiceFactory greetingServiceFactory) {
+		return greetingServiceFactory.createGreetingService("en");
+	}
+	
+	@Bean
+	@Primary
+	@Profile("es")
+	public GreetingService getSpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+		return greetingServiceFactory.createGreetingService("es");
+	}
+	
+	@Bean
+	@Primary
+	@Profile("de")
+	public GreetingService getGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
+		return greetingServiceFactory.createGreetingService("de");
+	}
+}
